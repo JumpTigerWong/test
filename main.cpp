fcfd0c62f1d5aee4fb5b1d52d8930fda95854f94
JumpTigerWong <38396114+JumpTigerWong@users.noreply.github.com>
2019-05-24 10:34:11 +0800

    拉格朗日插值
    计算机数值分析的拉格朗日插值用c++实现

--- main.cpp
+++ main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    double x[5]={165,123,150,123,141};//x
-    double lnx[5]={187,126,172,125,148};//y
-//    double x[5]={0.4,0.5,0.6,0.7,0.8};//x
-//    double lnx[5]={-0.9163,-0.6931,-0.5108,-0.3567,-0.2231};//y
-    double a,b,subx=0,suby=0,subx_2=0,subxy=0;
-    for(int i=0;i<5;i++){
-        subx+=x[i];
-        suby+=lnx[i];
-        subx_2+=x[i]*x[i];
-        subxy+=x[i]*lnx[i];
+int main()
+{
+    double x[5]={0.4,0.5,0.6,0.7,0.8};//x
+    double lnx[5]={-0.9163,-0.6931,-0.5108,-0.3567,-0.2231};//y
+    double y=0;
+    double num;
+    int a;
+    cout<<"请输入样本个数:";
+    cin>>a;
+    cout<<"请输入一个值:";
+    cin>>num;
+    for(int k=0;k<a;k++){
+        double t=1;
+        for(int i=0;i<a;i++){
+            if(i==k){
+                if(i==k&&k==a-1){
+                    break;
+                }
+                i++;
+                t*=(num-x[i])/(x[k]-x[i]);
+            }else{
+                t*=(num-x[i])/(x[k]-x[i]);
+            }
+        }
+        y+=t*lnx[k];
     }
-    b=(subxy-subx*suby/n)/(subx_2-subx*subx/n);
-    a=(suby-b*subx)/n;
-    cout<<"拟合函数为:"<<"y="<<b<<"x"<<a<<endl;
+    cout<<y<<endl;
 }

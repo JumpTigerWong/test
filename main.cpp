3efe47104bfb42bde33b932448812ecd59dadf3b
JumpTigerWong <38396114+JumpTigerWong@users.noreply.github.com>
2019-05-30 21:26:48 +0800

    龙贝格算法实现
    数值分析龙贝格公式c++实现

--- main.cpp
+++ main.cpp
@@ -3,31 +3,53 @@
 #include <algorithm>
 using namespace std;
 
-//double data[9]={1.0000000,0.9973978,0.9896158,0.9767267,0.9588510,0.9361556,0.9088516,0.8771925,0.8414709};
 double f(double x){
     if(x==0) return 1;
     else return (sin(x)/x);
 }
 
-double FuhuaTixing(int n,double a,double b){
-    double h = (b-a)/n;
-    double x = a;
-    double s = 0;
-    for(int k=0; k< n-1; k++){
-        x += h;
-        s += f(x);
-    }
-    double T = (f(a)+s*2+f(b))*h/2;
-    return T;
-}
-
-int main(){
-    cout<<"请输入积分区间(a,b)："<<endl;
-    double a;
-    double b;
+int main() {
+    double T[2]={0};
+    double S[2]={0};
+    double C[2]={0};
+    double R[2]={0,1000};
+    double a,b,e,h,x=0;
+    int Rk=0;
     cin>>a>>b;
-    cout<<"请输入等分份数n： "<<endl;
-    int n; cin>>n;
-    cout<<"由复化梯形公式球的结果："<<FuhuaTixing(n,a,b)<<endl;
+    cin>>e;
+    h=b-a;
+    T[1]=(f(a)+f(b))*h/2;
+    int k=1;
+    while (1){
+        S[0]=0;
+        x=a+h/2;
+        while(x<b){
+            S[0]+=f(x);
+            x+=h;
+        }
+        T[1]=(T[0]+S[0]*h)/2;
+        S[1]=T[1]+(T[1]-T[0])/3;
+        h/=2;
+        T[0]=T[1];
+        S[0]=S[1];
+        C[0]=C[1];
+        R[0]=R[1];
+        if(k==1){
+            k++;
+            continue;
+        }
+        C[1]=S[1]+(S[1]-S[0])/15;
+        if(k==2){
+            k++;
+            continue;
+        }
+        R[1]=C[1]+(C[1]-C[0])/63;
+        if(k==3){
+            k++;
+            continue;
+        }
+        if(abs(R[1]-R[0])<e)break;
+    }
+    cout<<k<<endl;
+    cout<<R[1]<<endl;
 }
-
